a6a75f1f36b0f8ad19cf07be29db4e324f2d8c3a
RyoSakon001 <ryo.development0320@gmail.com>
2025-10-17 04:31:18 +0000

    tailwindcss 導入

--- app/mysite/urls.py
+++ app/mysite/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
 
+if settings.DEBUG:
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
